7bfa7b251ac5922c6c3b7508d8601a041b66fe17
20cs122.rashmitha@sjec.ac.in <you@example.com>
2023-09-17 12:46:13 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const mongoose=require('mongoose')
 const body=require('body-parser');
-const { request } = require('http');
+
 const app=express()
 app.set('view engine', 'ejs');
 app.use(body.urlencoded({extended:true}))
